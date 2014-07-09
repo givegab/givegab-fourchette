4dbd7193bb8aeb5ddb6c548950160762eba8f39d
Ben Darfler <ben@givegab.com>
2014-07-09 16:46:41 -0400

    Skip pg copying

--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
 require 'fourchette'
 require_relative 'callbacks'
+require_relative 'heroku'
 run Sinatra::Application
---
+++ heroku.rb
@@ -0,0 +1,5 @@
+class Fourchette::Heroku
+  def copy_pg from, to
+    logger.info 'Skipping copy of posgres data'
+  end
+end
