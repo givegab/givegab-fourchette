e12c96a0339fbec94b9817efb9d92bae610ec6d1
Ben Darfler <ben@givegab.com>
2014-07-09 12:43:31 -0400

    Move callbacks to the proper location

--- callbacks.rb
+++ callbacks.rb
@@ -1,17 +1,39 @@
-# This is a sample file to see how the really, really basic callback system works.
-# See the README for me or just dive in.
 class Fourchette::Callbacks
   include Fourchette::Logger
 
   def initialize params
     @params = params
+    @heroku = Fourchette::Heroku.new
   end
 
   def before_all
-    logger.info 'Placeholder for before steps... (see callbacks.rb to override)'
+    logger.info 'Before callbacks...'
   end
 
   def after_all
-    logger.info 'Placeholder for after steps... (see callbacks.rb to override)'
+    logger.info 'After callbacks...'
+
+    case @params['action']
+      when 'closed'
+        logger.info "PR was closed..."
+      when 'opened', 'reopened'
+        logger.info "PR was reopened..."
+        logger.info "Setting BASE_URL to #{fork_url}"
+        @heroku.client.config_var.update(fork_name, {'BASE_URL' => 'fork_url'})
+    end
+  end
+
+  private
+
+  def heroku_fork
+    @heroku_fork ||= Fourchette::Fork.new(@params)
+  end
+
+  def fork_name
+    @fork_name ||= heroku_fork.fork_name
+  end
+
+  def fork_url
+    @fork_url ||= "http://#{fork_name}.herokuapp.com"
   end
 end
