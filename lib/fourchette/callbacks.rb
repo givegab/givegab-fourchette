475049383a5b6e23b940432f4da34573e3fcffa8
Ben Darfler <ben@givegab.com>
2014-07-09 12:36:48 -0400

    Add callback to update BASE_URL

---
+++ lib/fourchette/callbacks.rb
@@ -0,0 +1,32 @@
+class Fourchette::Callbacks
+  include Fourchette::Logger
+
+  def initialize params
+    @params = params
+    @heroku = Fourchette::Heroku.new
+  end
+
+  def before_all
+    logger.info 'Placeholder for before steps...'
+  end
+
+  def after_all
+    logger.info 'Placeholder for after steps...'
+    logger.info "Setting BASE_URL to #{fork_url}"
+    @heroku.client.config_var.update(fork_name, { 'BASE_URL' => 'fork_url' })
+  end
+
+  private
+
+  def heroku_fork
+    @heroku_fork ||= Fourchette::Fork.new(@params)
+  end
+
+  def fork_name
+    @fork_name ||= heroku_fork.fork_name
+  end
+
+  def fork_url
+    @fork_url ||= "http://#{fork_name}.herokuapp.com"
+  end
+end
