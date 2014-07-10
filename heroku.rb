50eb6795168a0d3e0bbc2332a38912f1ebb50699
Ben Darfler <ben@givegab.com>
2014-07-10 11:07:10 -0400

    fixing logic bug with testing for pg addon

--- heroku.rb
+++ heroku.rb
@@ -1,13 +1,15 @@
 class Fourchette::Heroku
   def copy_pg from, to
-    from_addons = client.addon.list(from)
-    pg_enabled = from_addons.any?{ |addon| addon['name'] == 'heroku-postgresql' }
-    if (pg_enabled)
+    if pg_enabled?(from)
       logger.info "Copying Postgres's data from #{from} to #{to}"
       backup = Fourchette::Pgbackups.new
       backup.copy(from, to)
     else
       logger.info "Postgres not enabled on #{from}. Skipping data copy."
     end
+  end
+
+  def pg_enabled?(app)
+    client.addon.list(app).any?{ |addon| addon['addon_service']['name'] == 'heroku-postgresql' }
   end
 end
