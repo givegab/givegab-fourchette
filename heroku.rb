7d282b2ad989856d67e467f56857a1b0f1fc59d2
Ben Darfler <ben@givegab.com>
2014-07-10 10:33:07 -0400

    Test for pg addon before copying pg data

--- heroku.rb
+++ heroku.rb
@@ -1,5 +1,13 @@
 class Fourchette::Heroku
   def copy_pg from, to
-    logger.info 'Skipping copy of posgres data'
+    from_addons = client.addon.list(from)
+    pg_enabled = from_addons.include?{ |addon| addon['name'] == 'heroku-postgresql' }
+    if (pg_enabled)
+      logger.info "Copying Postgres's data from #{from} to #{to}"
+      backup = Fourchette::Pgbackups.new
+      backup.copy(from, to)
+    else
+      logger.info "Postgres not enabled on #{from}. Skipping data copy."
+    end
   end
 end
